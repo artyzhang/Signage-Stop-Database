c91453585b9fa14affb81cc60b0035f38c16f4ca
Arty Zhang <arty.zhng@gmail.com>
2017-06-13 16:59:11 -0700

    updating merging database

--- preliminary_read.py
+++ preliminary_read.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 Location1 = r'N:\ServicePlanning\Signage Update\Signage-Stop-Database-master\StopDatabase_RoutesOnly.xlsx'
 stops_df = pd.read_excel(Location1)
+
+#
+
+print(stops_df.head())
