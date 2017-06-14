4b6a058f9e7b2ef875a12ee597920e0e6512b9b3
artyzhang <arty.zhng@gmail.com>
2017-06-13 17:39:48 -0700

    Update preliminary_read.py

--- preliminary_read.py
+++ preliminary_read.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
-Location1 = r'N:\ServicePlanning\Signage Update\Signage-Stop-Database-master\StopDatabase_RoutesOnly.xlsx'
+Location1 = r'N:\ServicePlanning\Signage Update\Signage-Stop-Database-master\StopIDV25_Trpz_merging_database.xlsx'
 stops_df = pd.read_excel(Location1)
 
-#
+# "Lines Served By Stops" = Column 19
+# "line 1 Name" = Column 25
 
-print(stops_df.head())
+print(stops_df.iloc[:5,[19] + list(range(25,37))])
