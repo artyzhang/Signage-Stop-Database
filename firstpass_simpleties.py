d75645e1f5d673ddf6de672cff674c7072692930
Arty Zhang <arty.zhng@gmail.com>
2017-06-14 18:12:51 -0700

    'Adding other simple cases'

--- firstpass_simpleties.py
+++ firstpass_simpleties.py
@@ -11,6 +11,11 @@ simpleroutes = ['1', '1AX','1BX','2', '3', '5R', '6', '7','7X', '8', '9R', '11',
 '25', '27', '28R','30X','31','31AX','31BX','35', '38AX','38BX','38R','39', '41','47', '49', '52', '54', '55', '56', '66', '67', '76X', '82X',
 '83X', '88','J','K','L','NX','T']
 
+one_direction_simple_only = ['8AX', '8BX', '37', '45', '81X', 'N']
+one_direction_simple_direction = {'8AX': 'OB','8BX': 'OB', '37': 'OB', '45':'OB', 'N':'OB'}
+
+owl_simple_only = ['44 owl', '48 owl', 'L owl', 'N owl']
+
 # Note to self: remember format is "testdf.loc[1,'Line 1 Name']"
 # find and replace
 
@@ -33,10 +38,17 @@ for k in Columnnumbers:
     stops_df.insert(co, k[:-5] + ' Route' , stops_df[k].where(stops_df[k].isin(simpleroutes), None))
 
 for j in Columnnumbers:
-    for ln in list(range(len(stops_df))):
+    for ln in range(len(stops_df)):
         if str(stops_df.loc[ln,j]) in simpleroutes:
             stops_df.loc[ln, followingcolumn(stops_df,j)] = str(stops_df.loc[ln, followingcolumn(stops_df,j)]) + '1'
             stops_df.loc[ln, columnafternext(stops_df,j)] = str(stops_df.loc[ln,j]) + str(stops_df.loc[ln,followingcolumn(stops_df,j)])
+
+#for m in Columnnumbers:
+#    for mn in range(len(stops_df)):
+#        if str(stops_df.loc[mn,m]) in one_direction_simple_only:
+#            if stops_df.loc[mn, followingcolumn(stops_df,m)] == one_direction_simple_direction[stops_df.loc[mn,m]]
+#               stops_df.loc[mn, followingcolumn(stops_df,m)] = str(stops_df.loc[mn, followingcolumn(stops_df,m)]) + '1'
+#               stops_df.loc[mn, columnafternext(stops_df,m)] = str(stops_df.loc[mn,m]) + str(stops_df.loc[ln,followingcolumn(stops_df,m)])
 
 Locationfinal = r'N:\ServicePlanning\Signage Update\TestFolder\StopDatabase_SimpleRoutesjoined.xlsx'
 stops_df.to_excel(Locationfinal, sheet_name = 'Sheet1', engine = 'xlsxwriter', index = False)
