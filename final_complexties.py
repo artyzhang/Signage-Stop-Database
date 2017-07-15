64154f8ece3753d0c76a68934491af45c1917a28
Zhang <Arty.Zhang@sfmta.com>
2017-07-14 19:25:44 -0700

    Adds instructions to final ties beta

--- final_complexties.py
+++ final_complexties.py
@@ -1,18 +1,46 @@
 import pandas as pd
 
-Location1 = r''
-Location2 = r''
+def columnplus2(df, dfcolumn1):
+    return df.columns.values[df.columns.get_loc(dfcolumn1)+2]
+
+def tryint(v):
+    try:
+        return int(v)
+    except ValueError:
+        return v
+
+def trystr(v):
+    try:
+        dummy = int(v)
+    except ValueError:
+        return True
+    return False
+
+Location1 = r"C:\Temp_CS\Signage-Stop-Database\StopIDv26Database_merging.xlsx"
+Location2 = r"C:\Temp_CS\Testfolder\stop_categorization.xlsx"
 stops_df = pd.read_excel(Location1)
 reference_df = pd.read_excel(Location2)
 
 columnlinenames = ['Line 1','Line 2','Line 3','Line 4', 'Line 5', 'Line 6', 'Line 7', 'Line 8', 'Line 9', 'Line 10', 'Line 11', 'Line 12', 'Line 13']
 
-reference_list = set(reference_dict['route'].tolist)
+# Create a nested dictionary object where the keys are a route number, paired to another dictionary
+# in which the stop ID's are keys that are paired to the appropriate code
+reference_list = [x.strip() if trystr(x) else x for x in set(reference_df['ROUTE_NAME'].tolist())]
 reference_dict = {}
-
-for j in range(len(reference_df)):
-  reference_dict[reference_df.loc[j, 'Stop ID']] = reference_dict.loc[j,'category']
+for d in reference_list:
+    nested_ref = {}
+    for j in range(len(reference_df)):
+        nested_ref[reference_df.loc[j, 'STOPID']] = reference_df.loc[j,'category']
+    reference_dict[d] = nested_ref
 
 for ind in range(7,len(stops_df)):
-  for name in columnlinenames:
-    if stops_df.loc[ind,name] =
+    for name in columnlinenames:
+        if stops_df.loc[ind,name] in reference_list:
+            lookup = stops_df.loc[ind,name]
+            if reference_dict[lookup][stops_df.loc[ind,"Stop ID"]] != '<Null>':
+                stops_df.loc[ind, columnplus2(stops_df, name)] = reference_dict[lookup][stops_df.loc[ind,"Stop ID"]]
+            else:
+                stops_df.loc[ind, columnplus2(stops_df, name)] = 'No Match'
+
+#Location3 =
+#stops_df.to_excel(Location3)
