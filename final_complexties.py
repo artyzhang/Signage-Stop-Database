757e31e3b32390a2b3a92adbd93fbcf8d9e0783c
Zhang <Arty.Zhang@sfmta.com>
2017-07-17 17:11:05 -0700

    Further updates to the complexties sheet, still has bugs

--- final_complexties.py
+++ final_complexties.py
@@ -9,8 +9,15 @@ def tryint(v):
     except ValueError:
         return v
 
+def trystr(v):
+     try:
+         dummy = int(v)
+     except ValueError:
+         return True
+     return False
+
 Location1 = r'N:\ServicePlanning\Signage Update\StopIDv26Database_merging.xlsx'
-Location2 = r''
+Location2 = r'N:\ServicePlanning\Signage Update\TestFolder\stop_categorization.xlsx'
 stops_df = pd.read_excel(Location1)
 reference_df = pd.read_excel(Location2)
 
@@ -18,22 +25,30 @@ columnlinenames = ['Line 1','Line 2','Line 3','Line 4', 'Line 5', 'Line 6', 'Lin
 
 # Create a nested dictionary object where the keys are a route number, paired to another dictionary
 # in which the stop ID's are keys that are paired to the appropriate code
-reference_list = [x.strip() if trystr(x) else x for x in set(reference_df['ROUTE_NAME'].tolist())]
+reference_list_len4 = list(set(reference_df['ROUTE_NAME'].tolist()))
+reference_list = [x.strip() if trystr(x) else x for x in reference_list_len4]
 reference_dict = {}
 for d in reference_list:
     nested_ref = {}
     for j in range(len(reference_df)):
-        nested_ref[reference_df.loc[j, 'STOPID']] = reference_df.loc[j,'category']
+        if reference_df.loc[j,'ROUTE_NAME'] == reference_list_len4[reference_list.index(d)]:
+            nested_ref[reference_df.loc[j, 'STOPID']] = reference_df.loc[j,'category']
     reference_dict[d] = nested_ref
 
 for ind in range(7,len(stops_df)):
     for name in columnlinenames:
-        if stops_df.loc[ind,name] in reference_list:
-            lookup = stops_df.loc[ind,name]
-            if reference_dict[lookup][stops_df.loc[ind,"Stop ID"]] != '<Null>':
-                stops_df.loc[ind, columnplus2(stops_df, name)] = reference_dict[lookup][stops_df.loc[ind,"Stop ID"]]
+        if tryint(stops_df.loc[ind,name]) in reference_list:
+            lookup = tryint(stops_df.loc[ind,name])
+            if stops_df.loc[ind,"Stop ID"] in reference_dict[lookup].keys():
+                if reference_dict[lookup][stops_df.loc[ind,"Stop ID"]] != '<Null>':
+                    stops_df.loc[ind, columnplus2(stops_df, name)] = reference_dict[lookup][stops_df.loc[ind,"Stop ID"]]
+                    #print('Match for '  + str(lookup) + ' and ' + str(stops_df.loc[ind,"Stop ID"]))
+                else:
+                    stops_df.loc[ind, columnplus2(stops_df, name)] = 'NULL'
+                    #print("Null for " + str(stops_df.loc[ind,"Stop ID"]))
             else:
                 stops_df.loc[ind, columnplus2(stops_df, name)] = 'No Match'
+                #print("No match for " + str(stops_df.loc[ind,"Stop ID"]) + " in reference list")
 
-#Location3 =
-#stops_df.to_excel(Location3)
+Location3 = r"N:\ServicePlanning\Signage Update\TestFolder\test_final_ties.xlsx"
+stops_df.to_excel(Location3)
