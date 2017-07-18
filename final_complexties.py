5caf8dcd1aba69dd662906268fbe6da86dcf2538
Arty Zhang <arty.zhng@gmail.com>
2017-07-18 01:14:41 -0700

    adding troubleshooting

--- final_complexties.py
+++ final_complexties.py
@@ -35,6 +35,7 @@ for d in reference_list:
             nested_ref[reference_df.loc[j, 'STOPID']] = reference_df.loc[j,'category']
     reference_dict[d] = nested_ref
 
+troubleshooting = []
 for ind in range(7,len(stops_df)):
     for name in columnlinenames:
         if tryint(stops_df.loc[ind,name]) in reference_list:
@@ -45,10 +46,23 @@ for ind in range(7,len(stops_df)):
                     #print('Match for '  + str(lookup) + ' and ' + str(stops_df.loc[ind,"Stop ID"]))
                 else:
                     stops_df.loc[ind, columnplus2(stops_df, name)] = 'NULL'
-                    #print("Null for " + str(stops_df.loc[ind,"Stop ID"]))
+                    troubleshooting.append(['Null',stops_df.loc[ind,name],stops_df.loc[ind,"Stop ID"]])
             else:
                 stops_df.loc[ind, columnplus2(stops_df, name)] = 'No Match'
-                #print("No match for " + str(stops_df.loc[ind,"Stop ID"]) + " in reference list")
+                troubleshooting.append(['No match',stops_df.loc[ind,name],stops_df.loc[ind,"Stop ID"]])
 
+print(troubleshooting)
 Location3 = r"N:\ServicePlanning\Signage Update\TestFolder\test_final_ties.xlsx"
-stops_df.to_excel(Location3)
+stops_df.loc[:,['Stop ID','Line 1','Line 1 Route',
+                       'Line 2','Line 2 Route',
+                       'Line 3','Line 3 Route',
+                       'Line 4','Line 4 Route',
+                       'Line 5','Line 5 Route',
+                       'Line 6','Line 6 Route',
+                       'Line 7','Line 7 Route',
+                       'Line 8','Line 8 Route',
+                       'Line 9','Line 9 Route',
+                       'Line 10','Line 10 Route',
+                       'Line 11','Line 11 Route',
+                       'Line 12','Line 12 Route'
+                       'Line 13','Line 13 Route']].to_excel(Location3)
